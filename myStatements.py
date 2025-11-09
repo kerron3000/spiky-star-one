eacc00929a20c6ef30a293a798c92f80630ec41a
kerron3000 <160296917+kerron3000@users.noreply.github.com>
2025-11-09 14:35:40 +0000

    adding files to repo

---
+++ myStatements.py
@@ -0,0 +1,9 @@
+my_list = ["apple", "cherry", "lemon", "pear"]
+
+my_dict = {"drink": "coffee", "milk": "10%"}
+
+for i in my_list:
+    print (f"I like eating: {i}")
+
+for k,v in my_dict.items():
+    print (f"Best {k} is {v}")
