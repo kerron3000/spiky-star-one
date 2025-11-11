b66c851c903bd984720b99ac9b480b4138caaad7
kerron3000 <160296917+kerron3000@users.noreply.github.com>
2025-11-11 01:35:21 +0000

    adding updated wikibot and mylib dir

--- wikibot.py
+++ wikibot.py
@@ -1,9 +1,22 @@
 import wikipedia
+import click
 
-usr_inp = input ("Please enter a name that you would like a wikipedia entry about: ")
 
-def scrape(name=usr_inp, lenght=1):
+@click.command()
+#@click.option('--count', default=1, help='Number of greetings.')
+@click.option('--name', prompt='Wikipedia page to scrape',
+              help='Webpage that we will scrape.')
+
+def scrape(name="Microsoft", lenght=1):
     res = wikipedia.summary(name, sentences=lenght)
-    return res
+    click.echo(f" {res}!")
     
 print (scrape())
+
+if __name__ == '__main__':
+    scrape()
+
+#usr_inp = input ("Please enter a name that you would like a wikipedia entry about: ")
+
+
+
